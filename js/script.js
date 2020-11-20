407fdee1a690f495c4fa3fc9dbc590b7be4a1183
lalakuQ <74050169+lalakuQ@users.noreply.github.com>
2020-11-20 14:07:08 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+ var name = 'Василий'; 
+  var admin = name;
+alert(admin);
+ var x1 = 1000
+var y1 = '108'
+console.log(x1 + y1);
+var x = +prompt('Введите число', 100);
+var x = +prompt('Введите число', 5);
+x = 100;
+y = 5;
+var c = x + y;
+var d = c + x - y;
+var e = d * x * y;
+var f = e / x / y;
+alert(f);
+var X = +prompt('Введите число', 345);
+var Y = +prompt('Введите число', 423);
+var Z = +prompt('Введите число', 189);
+var x = 345;
+var y = 423;
+var z = 189;
+var xyz = ((345 + 423 + 189) / 3);
+var res = xyz;
+alert(xyz)
+
+
+
